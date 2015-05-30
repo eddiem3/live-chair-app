18bb76819f339bf3fc311a0d5f7d5c46ac03db2c
Eddie Massey III <eem33@case.edu>
2015-05-29 21:53:23 -0400

    user can login incorrectly

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,36 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+    .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+	$scope.data = {};
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
+	$scope.login = function() {
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+		$state.go('tab.dash');
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+	}
+    })
+
+
+    .controller('DashCtrl', function($scope) {})
+
+    .controller('ChatsCtrl', function($scope, Chats) {
+	$scope.chats = Chats.all();
+	$scope.remove = function(chat) {
+	    Chats.remove(chat);
+	}
+    })
+
+    .controller('BarbersCtrl', function($scope, Barbers) {
+	$scope.barbers = Barbers.all();	
+    })
+
+
+    .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+	$scope.chat = Chats.get($stateParams.chatId);
+    })
+
+    .controller('AccountCtrl', function($scope) {
+	$scope.settings = {
+	    enableFriends: true
+	};
+    });
